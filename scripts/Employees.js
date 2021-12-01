c353048691d57cc8bb3ff5ef9e780bf1460c74b3
Ali Paradise <alison.paradise@gmail.com>
2021-12-01 14:18:45 -0600

    added click listener event for products sold

--- scripts/Employees.js
+++ scripts/Employees.js
@@ -1,12 +1,34 @@
-import { getEmployees } from "./database.js"
+import { getEmployees, getOrders } from "./database.js"
 
 const employees = getEmployees()
+const orders = getOrders()
 
-export const Employees = () => {
+document.addEventListener("click",(clickEvent) => {
+        const itemClicked = clickEvent.target
+        if (itemClicked.id.startsWith("employee")) {
+            const [, employeeId] = itemClicked.id.split("--")
+
+            for (const employee of employees) {
+                if (employee.id === parseInt(employeeId)) {
+
+                    const employeeOrders = orders.filter((order) => {  // <--- Go to YouTube and search "javascript array filter"
+                            if (order.employeeId === employee.id) {
+                                return true
+                            }
+                        }
+                    )
+
+                    window.alert(` ${employee.name} sold ${employeeOrders.length} products `)
+                }
+            }
+        }
+    })
+
+    export const Employees = () => {
     let html = "<ul>"
 
     for (const employee of employees) {
-        html += `<li id="employee--${employee}">${employee.name}</li>`
+        html += `<li id="employee--${employee.id}">${employee.name}</li>`
     }
 
     html += "</ul>"
